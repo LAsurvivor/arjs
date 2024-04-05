c15e5a5d265763851a1abb9bdff3402c3a1a20f8
Zhaohan Hu <lasurvivor1787@gmail.com>
2024-04-04 20:36:14 -0400

    update git ignore

--- node_modules/aframe/src/components/grabbable.js
+++
@@ -1,7 +0,0 @@
-var registerComponent = require('../core/component').registerComponent;
-
-registerComponent('grabbable', {
-  init: function () {
-    this.el.setAttribute('obb-collider', 'centerModel: true');
-  }
-});
